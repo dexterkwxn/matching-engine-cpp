824fd7af5f1e7fe7627d3117f08833c91c67b3a7
Dexter Kwxn <dexter.kwxn@gmail.com>
2023-02-11 14:35:05 +0800

    copy and move constructors for Instrument

--- engine.cpp
+++ engine.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <queue>
 #include <unordered_map>
+#include <mutex>
 
 #include "io.hpp"
 #include "engine.hpp"
@@ -13,17 +14,57 @@ struct Order {
 };
 
 struct Instrument {
+private:
   std::string instrument_name;
   std::priority_queue<Order> buy_queue;
   std::priority_queue<Order> sell_queue;
+  std::mutex instrument_mutex;
 
-  void match(Order order) {
+public:
+  Instrument() {}
+  Instrument(const Instrument& instrument_) { // copy constructor
+    buy_queue = instrument_.buy_queue;
+    sell_queue = instrument_.sell_queue;
+    instrument_name = instrument_.instrument_name;
   }
+  Instrument& operator=(const Instrument& instrument_) { // move constructor
+    buy_queue = instrument_.buy_queue;
+    sell_queue = instrument_.sell_queue;
+    instrument_name = instrument_.instrument_name;
+    return *this;
+  }
+
+  Instrument(std::string instrument_name_): instrument_name(instrument_name_) {}
+
+  void ProcessBuyOrder(uint32_t order_id, uint32_t price, uint32_t count) {
+    std::lock_guard guard(instrument_mutex);
+
+  }
+
 };
 
 struct OrderBook {
   std::unordered_map<std::string, Instrument> instruments;
+  std::mutex mutex;
+
+  OrderBook(): instruments(), mutex() {}
+
+  void InstrumentExists(std::string instrument) {
+    std::lock_guard guard(mutex);
+    if (instruments.contains(instrument)) {
+      return;
+    } else {
+      instruments[instrument] = Instrument(instrument);
+    }
+  }
+
+  void ProcessBuyOrder(uint32_t order_id, uint32_t price, uint32_t count, std::string instrument) {
+    InstrumentExists(instrument);
+    instruments[instrument].ProcessBuyOrder(order_id, price, count);
+  }
 };
+
+OrderBook order_book = OrderBook();
 
 void Engine::accept(ClientConnection connection)
 {
@@ -56,6 +97,10 @@ void Engine::connection_thread(ClientConnection connection)
 				Output::OrderDeleted(input.order_id, true, output_time);
 				break;
 			}
+
+      case input_buy:
+
+      case input_sell:
 
 			default: {
 				SyncCerr {}
